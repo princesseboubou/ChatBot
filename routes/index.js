bdd7dd4f0ef4b21848cc2ebc48dc29b1196ade9c
Marie Heurtevent <marieheurtevent@DN-MBA-1.local>
2018-02-13 20:27:30 +0100

    envoyer message (post?)

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var config = require('config');
 var router = express.Router();
+var chatService = require('../server/chatService');
 
 /* GET hello world page. */
 router.get('/', function(req, res, next) {
@@ -56,6 +57,33 @@ router.get('/webhook', (req, res) => {
       res.sendStatus(403);
     }
   }
+});
+
+// Creates the endpoint for our webhook
+router.post('/webhook', (req, res) => {
+
+  var body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the message. entry.messaging is an array, but
+      // will only ever contain one message, so we get index 0
+      var webhook_event = entry.messaging[0];
+
+      chatService.receivedMessage(webhook_event);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
 });
 
 module.exports = router;
